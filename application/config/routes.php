0de627e5d9751b0f812ea2d300d76c24cc580709
rizaldi <rizaldi354313@gmail.com>
2020-03-06 10:08:37 +0700

    config and route

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['export?group_id=1']	= 'export/index';
+$route['export/?group_id=1']	= 'export/index';
 
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
